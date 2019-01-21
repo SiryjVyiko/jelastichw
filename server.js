f07743f9a8aec773f44f99ddb5b350e8bc2394da
SiryjVyiko <dz@hivext.net>
2019-01-21 17:10:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var http = require("http"),
     ejs = require("ejs"),
     fs = require("fs"),
     staticResource = require("static-resource"),
-    port = 8080,
+    port = 8081,
     serverUrl,
     handler;
 
